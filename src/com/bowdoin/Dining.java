7dd17dabf44f74c10f4e620d24c715565b748802
Harrison Chapman <hchaps@gmail.com>
2011-02-06 16:03:49 -0500

    Changed around the buttons... made the title bar reflect the menus displayed. Possible performance loss? Or maybe just imagining things

--- src/com/bowdoin/Dining.java
+++ src/com/bowdoin/Dining.java
@@ -1,167 +1,182 @@
-
 package com.bowdoin;
 
-import android.app.TabActivity;
-import android.content.Context;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.TabHost.TabContentFactory;
-import android.widget.TabHost.TabSpec;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+public class Dining extends Activity {
+	protected static final int HALL_THORNE = 0;
+	protected static final int HALL_MOULTON = 1;
 
-public class Dining extends TabActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dining);
-        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-        setupTab(new TextView(this), "Breakfast");
-        setupTab(new TextView(this), "Lunch");
-        setupTab(new TextView(this), "Dinner");
-    }
+	protected static final int MEAL_BRUNCH = 0;
+	protected static final int MEAL_BREAKFAST = 1;
+	protected static final int MEAL_LUNCH = 2;
+	protected static final int MEAL_DINNER = 3;
 
-    private void setupTab(final View view, final String tag) {
-        View tabview = createTabView(mTabHost.getContext(), tag);
-        TabSpec setContent = mTabHost.newTabSpec(tag).
-            setIndicator(tabview).
-            setContent(
-                       new TabContentFactory() {
-                           public View createTabContent(String tag)
-                           { return view; }
-                       }
-                       );
-        mTabHost.addTab(setContent);
-    }
+	/** Called when the activity is first created. */
+	WebView mWebView;
 
-    private static View createTabView(final Context context,
-                                      final String text) {
-        View view = LayoutInflater.from(context).
-            inflate(R.layout.tabs_bg, null);
-        TextView tv = (TextView) view.findViewById(R.id.tabsText);
-        tv.setText(text);
-        return view;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dining);
+		mWebView = (WebView) findViewById(R.id.webview);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.getSettings().setBuiltInZoomControls(true);
+		mWebView.getSettings().setTextSize(WebSettings.TextSize.LARGER);
 
-    TabHost mTabHost;
+		mHall = Dining.HALL_THORNE;
+		mMeal = getCurrentMeal(mHall);
 
-    /** Called when the activity is first created. */
-	// WebView mWebView;
-	// @Override
+		mBreakfast = (SegmentedControlButton) findViewById(R.id.breakfast);
+		mBreakfast.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mMeal = Dining.MEAL_BREAKFAST;
+				updateView();
+			}
+		});
 
-    // public void onCreate(Bundle savedInstanceState) {
-    //     super.onCreate(savedInstanceState);
-    //     setContentView(R.layout.dining);
-    //     mWebView = (WebView) findViewById(R.id.webview);
-    //     mWebView.getSettings().setJavaScriptEnabled(true);
-    //     mWebView.getSettings().setBuiltInZoomControls(true);
-    //     mWebView.getSettings().setTextSize(WebSettings.TextSize.LARGER);
- 
-    //     mHall = "48";
-    //     mMeal = getCurrentMeal(mHall);
-        
-    //     mBreakfast = (Button) findViewById(R.id.breakfast);
-    //     mBreakfast.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mMeal = "Breakfast";
-    //         	updateView();
-    //         }
-    //     });
-        
-    //     mLunch = (Button) findViewById(R.id.lunch);
-    //     mLunch.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mMeal = "Lunch";
-    //         	updateView();
-    //         }
-    //     });
-        
-    //     mDinner = (Button) findViewById(R.id.dinner);
-    //     mDinner.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mMeal = "Dinner";
-    //         	updateView();
-    //         }
-    //     });
-        
-    //     mBrunch = (Button) findViewById(R.id.brunch);
-    //     mBrunch.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mMeal = "Brunch";
-    //         	updateView();
-    //         }
-    //     });
-        
-    //     mMoulton = (Button) findViewById(R.id.moulton);
-    //     mMoulton.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mHall = "48";
-    //         	updateView();
-    //         }
-    //     });
-        
-    //     mThorn = (Button) findViewById(R.id.thorne);
-    //     mThorn.setOnClickListener(new View.OnClickListener() {
-    //         public void onClick(View v) {
-    //         	mHall = "49";
-    //         	updateView();
-            	
-    //         }
-    //     });
+		mLunch = (SegmentedControlButton) findViewById(R.id.lunch);
+		mLunch.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mMeal = Dining.MEAL_LUNCH;
+				updateView();
+			}
+		});
 
-    //     updateView();
-        
-    // }
+		mDinner = (SegmentedControlButton) findViewById(R.id.dinner);
+		mDinner.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mMeal = Dining.MEAL_DINNER;
+				updateView();
+			}
+		});
+
+		mBrunch = (SegmentedControlButton) findViewById(R.id.brunch);
+		mBrunch.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mMeal = Dining.MEAL_BRUNCH;
+				updateView();
+			}
+		});
+
+		mMoulton = (SegmentedControlButton) findViewById(R.id.moulton);
+		mMoulton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mHall = Dining.HALL_MOULTON;
+				updateView();
+			}
+		});
+
+		mThorn = (SegmentedControlButton) findViewById(R.id.thorne);
+		mThorn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mHall = Dining.HALL_THORNE;
+				updateView();
+
+			}
+		});
+
+		updateView();
+
+	}
+
+	// Roughly guesses which meal is currently going on
+	// easy enough to make this correct per-hall with some sort of
+	// scheduling object
+	private int getCurrentMeal(int hall) {
+		Calendar calendar = GregorianCalendar
+				.getInstance(TimeZone.getDefault());
+		int hours = calendar.get(Calendar.HOUR_OF_DAY);
+		int meal;
+
+		if (hours > 14)
+			meal = Dining.MEAL_DINNER;
+		else if (hours > 10)
+			meal = Dining.MEAL_LUNCH;
+		else
+			meal = Dining.MEAL_BREAKFAST;
+
+		return meal;
+	}
+
+	private void updateView() {
+		updateTitle();
+		loadUrl();
+	}
 	
-	// // Roughly guesses which meal is currently going on
-	// // easy enough to make this correct per-hall with some sort of
-	// // scheduling object
-	// private String getCurrentMeal(String hall) {
-	// 	Calendar calendar = GregorianCalendar.getInstance(TimeZone.getDefault());
-	// 	int hours = calendar.get(Calendar.HOUR_OF_DAY);
-	// 	String meal;
-		
-	// 	if (hours > 14)
-	// 		meal = "Dinner";
-	// 	else if (hours > 10)
-	// 		meal = "Lunch";
-	// 	else
-	// 		meal = "Breakfast";
-	
-	// 	return meal;
-	// }
-	
-	// private void updateView() {
-    //     loadUrl();
-	// }
+	private void updateTitle() {
+		setTitle(getHallString(mHall).concat(" - ").concat(getMealString(mMeal)));
+	}
 
-	// private void loadUrl() {
-	// 	String Url = "http://www.bowdoin.edu/atreus/views?unit=" .concat(mHall).concat("&meal=").concat(mMeal);
-	// 	mWebView.clearView();
-	// 	mWebView.loadData("Loading...", "text/html", "");
-	// 	mWebView.refreshDrawableState();
-	// 	mWebView.loadUrl(Url);
-	// }
+	private void loadUrl() {
+		String Url = "http://www.bowdoin.edu/atreus/views?unit=".concat(
+				getHallId(mHall)).concat("&meal=").concat(
+				getMealString(mMeal));
+		mWebView.clearView();
+		mWebView.loadData("Loading...", "text/html", "");
+		mWebView.refreshDrawableState();
+		mWebView.loadUrl(Url);
+	}
+
+	protected String getHallString(int hall) {
+		switch (hall) {
+		case Dining.HALL_MOULTON:
+			return "Moulton Union";
+		case Dining.HALL_THORNE:
+			return "Thorne Hall";
+		}
+		return null;
+	}
+
 	
-	// protected void onSaveInstanceState(Bundle outState){
-	// 	outState.putString("mMeal", mMeal);
-	// 	outState.putString("mHall", mHall);
-	// }
-	
-	// protected void onRestoreInstanceState(Bundle inState){
-	// 	mMeal = inState.getString("mMeal");
-	// 	mHall = inState.getString("mHall");
-	// 	updateView();
-	// }
-	
-	// private Button mBreakfast;
-	// private Button mLunch;
-	// private Button mDinner;
-	// private Button mBrunch;
-	// private Button mMoulton;
-	// private Button mThorn;
-	// private String mMeal;
-	// private String mHall;
+	protected String getHallId(int hall) {
+		switch (hall) {
+		case Dining.HALL_MOULTON:
+			return "48";
+		case Dining.HALL_THORNE:
+			return "49";
+		}
+		return null;
+	}
+
+	protected String getMealString(int meal) {
+		switch(meal) {
+		case Dining.MEAL_BREAKFAST:
+			return "Breakfast";
+		case Dining.MEAL_LUNCH:
+			return "Lunch";
+		case Dining.MEAL_DINNER:
+			return "Dinner";
+		case Dining.MEAL_BRUNCH:
+			return "Brunch";
+		}
+		return null;
+	}
+
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putInt("mMeal", mMeal);
+		outState.putInt("mHall", mHall);
+	}
+
+	protected void onRestoreInstanceState(Bundle inState) {
+		mMeal = inState.getInt("mMeal");
+		mHall = inState.getInt("mHall");
+		updateView();
+	}
+
+	private SegmentedControlButton mBreakfast;
+	private SegmentedControlButton mLunch;
+	private SegmentedControlButton mDinner;
+	private SegmentedControlButton mBrunch;
+	private SegmentedControlButton mMoulton;
+	private SegmentedControlButton mThorn;
+	private int mMeal;
+	private int mHall;
 }
--- src/com/bowdoin/SegmentedControlButton.java
+++ src/com/bowdoin/SegmentedControlButton.java
@@ -1,79 +1,102 @@
 package com.bowdoin;
+
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.Paint.Style;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.GradientDrawable.Orientation;
 import android.util.AttributeSet;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 public class SegmentedControlButton extends RadioButton {
 
-    private float mX;
+	private float mX;
 
-    public SegmentedControlButton(Context context) {
-        super(context);
-    }
+	public SegmentedControlButton(Context context) {
+		this(context, null);
+		// setButtonDrawable(android.R.drawable.btn_default);
+	}
 
-    public SegmentedControlButton(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public SegmentedControlButton(Context context, AttributeSet attrs) {
+		this(context, attrs, android.R.attr.buttonStyle);
+		// setButtonDrawable(android.R.drawable.btn_default);
+	}
 
-    public SegmentedControlButton(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+	public SegmentedControlButton(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		// setButtonDrawable(android.R.drawable.btn_default);
+	}
 
-    private static final float TEXT_SIZE = 16.0f;
+	// private static final float TEXT_SIZE = 16.0f;
 
-    @Override
-    public void onDraw(Canvas canvas) {
+//	@Override
+//	public void onDraw(Canvas canvas) {
+//		super.onDraw(canvas);
+//		
+//		if (isChecked()) {
+//			setSelected(true);
+//		} else {
+//			setSelected(false);
+//		}
+//	}
 
-        String text = this.getText().toString();
-        Paint textPaint = new Paint();
-        textPaint.setAntiAlias(true);
-        float currentWidth = textPaint.measureText(text);
-        float currentHeight = textPaint.measureText("x");
+	@Override
+	protected int[] onCreateDrawableState(int extraSpace) {
+		int[] states;
+		if (isChecked()) {
+			states = Button.SELECTED_STATE_SET;
+		} else {
+//			if (hasFocus()) {
+				states = super.onCreateDrawableState(extraSpace);
+	//		} else {
+		//		states = Button.EMPTY_STATE_SET;
+			//}
+		}
+		return states;
+	}
+	
+	/*
+	 * @Override public void onDraw(Canvas canvas) {
+	 * 
+	 * String text = this.getText().toString(); Paint textPaint = new Paint();
+	 * textPaint.setAntiAlias(true); //float currentWidth =
+	 * textPaint.measureText(text); float currentHeight =
+	 * textPaint.measureText("x");
+	 * 
+	 * // final float scale = //
+	 * getContext().getResources().getDisplayMetrics().density; // float
+	 * textSize = (int) (TEXT_SIZE * scale + 0.5f);
+	 * textPaint.setTextSize(this.getTextSize());
+	 * textPaint.setTextAlign(Paint.Align.CENTER);
+	 * 
+	 * //float canvasWidth = canvas.getWidth(); //float textWidth =
+	 * textPaint.measureText(text);
+	 * 
+	 * if (isChecked()) { //NinePatchDrawable bg =
+	 * getDrawable(android.R.drawable.btn_default);
+	 * setButtonDrawable(android.R.drawable.btn_default);
+	 * 
+	 * // GradientDrawable grad = new GradientDrawable(Orientation.BOTTOM_TOP,
+	 * new int[] { 0xffdcdcdc, 0xff111111 }); // grad.setBounds(0, 0,
+	 * this.getWidth(), this.getHeight()); // grad.draw(canvas); //
+	 * textPaint.setColor(Color.WHITE); } else { GradientDrawable grad = new
+	 * GradientDrawable(Orientation.TOP_BOTTOM, new int[] { 0xffa5a5a5,
+	 * 0xff000000 }); grad.setBounds(0, 0, this.getWidth(), this.getHeight());
+	 * grad.draw(canvas); textPaint.setColor(0xffcccccc); }
+	 * 
+	 * //float w = (this.getWidth() / 2) - currentWidth; float h =
+	 * (this.getHeight() / 2) + currentHeight; canvas.drawText(text, mX, h,
+	 * textPaint);
+	 * 
+	 * Paint paint = new Paint(); paint.setColor(Color.BLACK);
+	 * paint.setStyle(Style.STROKE); Rect rect = new Rect(0, 0, this.getWidth(),
+	 * this.getHeight()); canvas.drawRect(rect, paint);
+	 * 
+	 * }
+	 */
 
-        // final float scale =
-        // getContext().getResources().getDisplayMetrics().density;
-        // float textSize = (int) (TEXT_SIZE * scale + 0.5f);
-        textPaint.setTextSize(this.getTextSize());
-        textPaint.setTextAlign(Paint.Align.CENTER);
-
-        float canvasWidth = canvas.getWidth();
-        float textWidth = textPaint.measureText(text);
-
-        if (isChecked()) {
-            GradientDrawable grad = new GradientDrawable(Orientation.TOP_BOTTOM, new int[] { 0xffdcdcdc, 0xff111111 });
-            grad.setBounds(0, 0, this.getWidth(), this.getHeight());
-            grad.draw(canvas);
-            textPaint.setColor(Color.WHITE);
-        } else {
-            GradientDrawable grad = new GradientDrawable(Orientation.TOP_BOTTOM, new int[] { 0xffa5a5a5, 0xff000000 });
-            grad.setBounds(0, 0, this.getWidth(), this.getHeight());
-            grad.draw(canvas);
-            textPaint.setColor(0xffcccccc);
-        }
-
-        float w = (this.getWidth() / 2) - currentWidth;
-        float h = (this.getHeight() / 2) + currentHeight;
-        canvas.drawText(text, mX, h, textPaint);
-
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Style.STROKE);
-        Rect rect = new Rect(0, 0, this.getWidth(), this.getHeight());
-        canvas.drawRect(rect, paint);
-
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int ow, int oh) {
-        super.onSizeChanged(w, h, ow, oh);
-        mX = w * 0.5f; // remember the center of the screen
-    }
+	@Override
+	protected void onSizeChanged(int w, int h, int ow, int oh) {
+		super.onSizeChanged(w, h, ow, oh);
+		mX = w * 0.5f; // remember the center of the screen
+	}
 
 }
